7e7dfda3de78349446f273c4bdec55b043231f48
HimanshuGupta0317 <himanshugupta07052002@gmail.com>
2025-01-20 11:48:59 +0530

    Merge all compo

--- src/Components/AdminDashboard/Dashboard.jsx
+++ src/Components/AdminDashboard/Dashboard.jsx
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
-// import RecentTour from './RecentTour';
-// import Chart from 'react-apexcharts'
-// import RecentActivity from './RecentActivity';
 import DashboardPage from './DashboardPage';
-import PackageForm from './ClassRoom';
 // import { Package, Users, MapPin, Calendar, MoreVertical } from 'lucide-react';
 import ClassRoom from './ClassRoom';
 import MentorsMain from './MentorsMain';
 import EventsMain from './EventsMain';
 import SettingMain from './SettingMain';
+import Messages from './Message/Messages';
+import Comunity from './Community/Comunity'
+import Payments from './Payments'
+import Task from './MYTask/Task'
 
 
 function Dashboard({ activeTab }) {
 
-
-    // const stats = [
-    //     { label: 'Total Packages', value: '24', icon: <Package className="h-6 w-6 text-blue-600" /> },
-    //     { label: 'Active Bookings', value: '156', icon: <Calendar className="h-6 w-6 text-green-600" /> },
-    //     { label: 'Total Customers', value: '1,204', icon: <Users className="h-6 w-6 text-purple-600" /> },
-    //     { label: 'Destinations', value: '18', icon: <MapPin className="h-6 w-6 text-orange-600" /> },
-    // ];
 
     return (
         <div className=' '>
@@ -59,6 +52,12 @@ function Dashboard({ activeTab }) {
                     <div>
                         <h2 className="text-2xl font-semibold mb-6 text-indigo-700">Add New Package</h2>
                         <MentorsMain />
+                    </div>
+                )}
+                 {activeTab === 'payments' && (
+                    <div>
+                        <h2 className="text-2xl font-semibold mb-6 text-indigo-700">Add New Package</h2>
+                        <Payments/>
                     </div>
                 )}
 
